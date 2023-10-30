8bdaffe6a6a65327edaf09965c164c6e73dfde6b
wycliffe <ericoochieng456@gmail.com>
2023-10-30 19:57:50 +0300

    register

--- dbservice.py
+++ dbservice.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from sqlalchemy import Column, Integer, ForeignKey, String, Numeric
+# from sqlalchemy import Column, Integer, ForeignKey, String, Numeric
 from sqlalchemy.orm import relationship
 from sqlalchemy import DateTime
 
@@ -8,72 +8,78 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:2345@localhost:5432/duka"
 db = SQLAlchemy(app)
 
-class Products(db.Model):
-    __tablename__ = 'products'
-    product_id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(255), nullable=False)
-    buying_price = db.Column(db.Numeric(precision=15, scale=2), nullable=False)
-    selling_price = db.Column(db.Numeric(precision=15, scale=2))
-    stock_quantity = db.Column(db.Numeric(precision=15, scale=2))
-    sales_details = relationship("SaleDetails", back_populates="product")
-
-class SaleDetails(db.Model):
-    __tablename__ = 'sale_details'
-    id = db.Column(db.Integer, primary_key=True)
-    sale_id = db.Column(db.Integer, db.ForeignKey('sales.sale_id'))
-    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'))
-    quantity = db.Column(db.Numeric(precision=15, scale=2))
-    purchase_amount = db.Column(db.Numeric(precision=15, scale=2))
-    product = relationship("Products", back_populates="sales_details")
+# class Products(db.Model):
+#     __tablename__ = 'products'
+#     product_id = db.Column(db.Integer, primary_key=True)
+#     product_name = db.Column(db.String(255), nullable=False)
+#     buying_price = db.Column(db.Numeric(precision=15, scale=2), nullable=False)
+#     selling_price = db.Column(db.Numeric(precision=15, scale=2))
+#     stock_quantity = db.Column(db.Numeric(precision=15, scale=2))
+#     sales_details = relationship("SaleDetails", back_populates="product") 
 
 
-class Customers (db.Model):
-    __tablename__ = 'customers'
-    customer_id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(255))
-    phone_no = db.Column(db.String(13), nullable=False)
-    email = db.Column(db.String(255))
-    sales = relationship("Sales", back_populates="customers")
-    payments = relationship("Payments", back_populates="customers")
+# class SaleDetails(db.Model):
+#     __tablename__ = 'sale_details'
+#     id = db.Column(db.Integer, primary_key=True)
+#     sale_id = db.Column(db.Integer, db.ForeignKey('sales.sale_id'))
+#     product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'))
+#     quantity = db.Column(db.Numeric(precision=15, scale=2))
+#     purchase_amount = db.Column(db.Numeric(precision=15, scale=2))
+#     product = relationship("Products", back_populates="sales_details") 
+#     sales = relationship("Sales", back_populates="sale_details") 
 
 
-class Employees(db.Model):
-    __tablename__ = 'employees'
-    employee_id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255))
-    contact = db.Column(db.String(13), nullable=False)
-    position = db.Column(db.String(255), nullable=False)
-    users = relationship("Users", back_populates="employees")
+# class Customers (db.Model):
+#     __tablename__ = 'customers'
+#     customer_id = db.Column(db.Integer, primary_key=True)
+#     full_name = db.Column(db.String(255))
+#     phone_no = db.Column(db.String(13), nullable=False)
+#     email = db.Column(db.String(255))
+#     sales = relationship("Sales", back_populates="customers") 
+#     payments = relationship("Payments", back_populates="customers") 
+
+
+# class Employees(db.Model):
+#     __tablename__ = 'employees'
+#     employee_id = db.Column(db.Integer, primary_key=True)
+#     full_name = db.Column(db.String(255), nullable=False)
+#     email = db.Column(db.String(255))
+#     contact = db.Column(db.String(13), nullable=False)
+#     position = db.Column(db.String(255), nullable=False)
+#     # users = relationship("Users", back_populates="employees") 
 
 
 class Users(db.Model):
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'))
-    password = db.Column(db.String(255), nullable=False)
-    user_email = db.Column(db.String(255), nullable=False, unique=True)
-    sales = relationship("Sales", back_populates="users")
-
-
-class Sales(db.Model):
-    __tablename__ = 'sales'
-    sale_id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    total_amount = db.Column(db.Numeric(precision=15, scale=2))
-    created_at = db.Column(DateTime, default=db.func.current_timestamp())
-    sales_details = relationship("SaleDetails", back_populates="sales")
-    payments = relationship("Payments", back_populates="sales")
-
-
-class Payments(db.Model):
-    __tablename__ = 'payments'
     id = db.Column(db.Integer, primary_key=True)
-    sale_id = db.Column(db.Integer, db.ForeignKey('sales.sale_id'))
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'))
-    payment_method = db.Column(db.String(255))
-    amount = db.Column(db.Numeric(precision=15, scale=2))
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    # sales = relationship("Sales", back_populates="users") 
+    # employees = relationship("Employees", back_populates="users") 
+    
+
+# class Sales(db.Model):
+#     __tablename__ = 'sales'
+#     sale_id = db.Column(db.Integer, primary_key=True)
+#     customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'))
+#     # user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+#     # i think payement id is not here
+#     total_amount = db.Column(db.Numeric(precision=15, scale=2))
+#     created_at = db.Column(DateTime, default=db.func.current_timestamp())
+#     sale_details = relationship("SaleDetails", back_populates="sales") 
+#     # users = relationship("Users", back_populates="sales") 
+#     customers = relationship("Customers", back_populates="sales") 
+    
+
+# class Payments(db.Model):
+#     __tablename__ = 'payments'
+#     id = db.Column(db.Integer, primary_key=True)
+#     sale_id = db.Column(db.Integer, db.ForeignKey('sales.sale_id'))
+#     customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'))
+#     payment_method = db.Column(db.String(255))
+#     amount = db.Column(db.Numeric(precision=15, scale=2))
+#     customers = relationship("Customers", back_populates="payments") 
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
-from flask import render_template,session,request,url_for,redirect
+from flask import render_template,session,request,url_for,redirect,flash
 from dbservice import *
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 app.secret_key="Techcamp"
@@ -10,9 +12,27 @@ def advanced_system():
     return render_template("index.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    return redirect(url_for("login"))
+    if request.method=="POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        hashed_pass=generate_password_hash(password)
+        # check email existence
+        user=Users.query.filter_by(email=email).first()
+        print("User")
+
+        if user:
+            print("")
+            flash("Email already exist")
+        else:
+            new_user=Users(email=email,password=hashed_pass)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("You have succesfully registered")
+            return redirect(url_for('login'))
+
+    return render_template("register.html")
 
 
 @app.route("/dashboard")
@@ -34,8 +54,10 @@ def products():
 
 @app.route("/sales")
 def sales():
-    return render_template("sales.html")
+    records=Sales.query.all()
+    sales=[sale for sale in records]
+    return render_template("sales.html", sales=sales)
 
 
 if __name__== "__main__":
-    app.run()
+    app.run(debug=True)
